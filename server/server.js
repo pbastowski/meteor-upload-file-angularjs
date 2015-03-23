c08169c6ea99dfe4ed31cd9ec3685d7acf9255f3
Bastowski, PL (ITCDCC) - KLM <Paul.Bastowski@klm.com>
2015-03-23 14:57:56 +0100

    [x] removed fileModel directive - just using the angular.element(this) way to detech input[type=file] changes [x] cleaned up the code

--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
 Images = new FS.Collection('images', {
     //stores: [new FS.Store.FileSystem("images", {path: "../../../../../.uploads"})]
-    //stores: [new FS.Store.FileSystem("images", {path: "../../../../../server/uploads"})]
-    //stores: [new FS.Store.FileSystem("images", {path: "./uploads"})]
     stores: [new FS.Store.GridFS('images')]
 });
 
